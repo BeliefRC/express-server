cbb7099dddeb7e8a36b5689753dd6d2579f6768d
BeliefRC <beliefrc@outlook.com>
2019-06-13 11:02:54 +0800

    feat 新增代理与超时处理的功能

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express')
+const timeout = require('connect-timeout')
+const proxy = require('http-proxy-middleware')
 const dist = 'dist'
 const app = express()
+
+// 超时时间
+const TIME_OUT = 30 * 1e3
+
+// 设置超时 返回超时响应
+app.use(timeout(TIME_OUT))
+
+app.use((req, res, next) => {
+  if (!req.timedout) next()
+})
+
+app.use(proxy(['/tenant/', '/notify/', '/frontend/', '/kb/', '/cmdb/'], {target: 'http://newprod.test.cn'}))
+app.use(proxy('/api/v2', {target: 'http://newprod.test.cn/itsm'}))
+
+app.use(express.static(dist))
+
 app.get('/', (req, res) => {
   res.sendfile(`${__dirname}/${dist}/index.html`)
 })
-
-app.use(express.static(dist))
 app.listen('1234', () => {
   console.log(`server is running at port 1234`)
 })
